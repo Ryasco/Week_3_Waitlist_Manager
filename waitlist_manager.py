a0bbfe16fdc8ec3b6e0db2298df81349144ee07e
Ryan Scott <ryanscott@Ryans-iMac.local>
2026-09-16 10:44:46 -0500

    Debugged final output issues

--- waitlist_manager.py
+++ waitlist_manager.py
@@ -118,7 +118,20 @@ waitlist_generator()
 
 '''
 Design Memo: Write Your Design Memo Include a 200–300 word response in your code or in a .txt file:
-- How does your list work?
-- What role does the head play?
-- When might a real engineer need a custom list like this?
+- My list works by using commands prompted in the terminal (one through five) to add, edit or 
+remove customers from the waitlist. Each customer is represented by a node which stores the 
+customer’s name and reference to the next node in the list. Once the command is chosen, it 
+adds, edits or removes the customer’s name to the list or if the command is to print, it will 
+print the current list or if the command is to exit, it will exit the list. In the case where 
+there are no matching names and a request to remove a name is commanded, the system will prompt 
+that the name isn’t found or if the user is trying to print an empty list, it will advise the 
+list is empty and both options will return the user to the main menu. The “head” is the 
+beginning of the list, which is where you start when looking for information within a list. 
+An engineer might need a custom list like this when the information on a list needs to be 
+reorganized and the items on the list can be removed and added. One example that comes to mind 
+for me is when I worked at a meat market and I made a list of products that were close to 
+expiring. This list changes weekly and can change depending on what we would get on the truck. 
+As the product sells, it is removed from the list and as time goes on, more products get added 
+to the list and the order can change depending on if we would get a product that is older than 
+what we have already or if there is overstock on meat that was already cut. 
 '''
